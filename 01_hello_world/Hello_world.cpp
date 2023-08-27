470dab4a4b2f6658cb44d92dafa291787ac42321
Pragnesh Patel <patelpragnesh036@gmail.com>
2023-08-27 15:23:33 +0530

    Hello world demo

---
+++ 01_hello_world/Hello_world.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Hello world.cpp
+// Author      : Pragnesh Patel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Testing code for c++
+//============================================================================
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "hello my name is pragnesh " << endl << \
+    "I am form INDIA " << endl << \
+    "I am working as Embedded Firmware Devloper "<< endl ;
+    return 0;
+}
+
