a6b9924afa24d276ae71e9c1ba8ef62036a92e74
pragneshpatel2023 <patelpragnesh036@gmail.com>
2023-09-10 17:43:05 +0530

    -> Added code to undestnd the use of flash in C++.

---
+++ 02_Use_of_flash/Use_of_flash.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#define TEST_FLUSH  0
+
+using namespace std;
+
+
+int main( void )
+{
+    uint32_t loop = 0;
+    for( loop =1; loop <= 5; loop++ )
+    {
+#if ( TEST_FLUSH == 1 )        
+        cout << loop << " " << flush;
+#else
+        cout << loop << " ";
+#endif
+        this_thread::sleep_for( chrono::seconds(1) ); 
+    }
+    cout << endl;
+}
+
