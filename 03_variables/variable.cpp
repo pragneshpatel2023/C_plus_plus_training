b8947d4c07709af9d4788091d98c92d316427509
Pragneshpatel2023 <patelpragnesh036@gmail.com>
2023-11-14 18:27:00 +0530

    C++ variables

---
+++ 03_variables/variable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int numberOfDogs = 5;
+    int numberOfCats = 10;
+    
+    cout << "Number of Dog's " << numberOfDogs << endl;
+
+    cout << "Number of Cat's " << numberOfCats << endl;
+
+    cout << "Total Animals = " << numberOfCats + numberOfDogs << endl;
+    
+    return 0;
+}
+
